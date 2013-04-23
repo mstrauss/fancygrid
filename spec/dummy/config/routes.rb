ab9bbe25d9a45c3bf5f0a6a05460b3d252ba32de
Markus Strauss <Markus@ITstrauss.eu>
2013-04-23 23:06:30 +0200

    fixed routes for rails 4

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -49,7 +49,11 @@ Dummy::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "application#index"
-  match "/index(.:format)", :to => "application#index"
+  if Rails::VERSION::STRING > '4.0.0'
+    get "/index(.:format)", :to => "application#index"
+  else
+    match "/index(.:format)", :to => "application#index"
+  end
   
   # See how all your routes lay out with "rake routes"
 
